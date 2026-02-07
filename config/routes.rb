986838052add069c4052254d2d31fd9defd07fb6
WeslleyHarielLima <weslleyhariellima@gmail.com>
2026-02-07 10:53:17 -0400

    feat: Dashboard inicial com cards de resumo e menu mobile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
+  
+  authenticated :user do
+    root 'dashboard#index', as: :authenticated_root
+  end
+  
+  resources :dashboard, only: [:index]
 end
