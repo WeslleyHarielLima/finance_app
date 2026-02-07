742950d0e04186ae6555ae4d1bfbc12db8925065
WeslleyHarielLima <weslleyhariellima@gmail.com>
2026-02-07 10:58:42 -0400

    feat:routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  # Rota raiz baseada no login
   unauthenticated do
     root 'pages#home'
   end
@@ -10,10 +9,7 @@ Rails.application.routes.draw do
     root 'dashboard#index', as: :authenticated_root
   end
   
-  # Rotas do app
   get 'dashboard', to: 'dashboard#index'
   resources :financial_entries
-  
-  # Rota fallback para desenvolvimento
   get 'pages/home'
 end
